6934d6c9572ab46ddb868f1a253a9764cdf2c581
poli8512 <poli8512@gmail.com>
2023-02-04 18:44:36 +0300

    feat webpack and vue

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>WEB Vue</div>
+<!--    <weather-widget/>-->
+<!--    <div>App{{$store.state.count}}</div>-->
+</template>
+<script lang="ts">
+	// import {useStore} from "vuex"
+	// import WeatherWidget from './components/WeatherWidget.vue'
+
+	export default {
+		// components: {WeatherWidget},
+		// setup():void {
+		// 	const store = useStore()
+		// 	store.dispatch('getData')
+		// },
+	}
+</script>
+<style scoped>
+</style>
