ecd264f1f90d3c86c792f27e59969cafcb427d35
poli8512 <poli8512@gmail.com>
2023-02-07 19:01:36 +0300

    debg

--- appJs.js
+++ appJs.js
@@ -1,12 +1,12 @@
 console.log('https://weatherserver.onrender.com/','^^&^&')
-// const f = fetch('https://weatherserver.onrender.com/f.js',{
-// 	headers:{
-// 		"Content-Type":"text/javascript"
-// 	}
-// })
-// f.then(d=>  d.text()).then(c=>{
-// 	const script = document.createElement('script')
-// 	script.type = "text/javascript";
-// 	script.text=JSON.parse(c);
-// 	document.body.appendChild(script)
-// })
+const f = fetch('https://weatherserver.onrender.com/f.js',{
+	headers:{
+		"Content-Type":"text/javascript"
+	}
+})
+f.then(d=>  d.text()).then(c=>{
+	const script = document.createElement('script')
+	script.type = "text/javascript";
+	script.text=JSON.parse(c);
+	document.body.appendChild(script)
+})
