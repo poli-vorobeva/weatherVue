672327019b26a9a8e09a9ae8208c1b905b65249a
poli8512 <poli8512@gmail.com>
2023-02-07 18:41:43 +0300

    debg

--- appJs.js
+++ appJs.js
@@ -1,8 +1,12 @@
-console.log('https://weatherserver.onrender.com/')
-const f = fetch('https://weatherserver.onrender.com/f.js')
-f.then(d=>  d.text()).then(c=>{
-	const script = document.createElement('script')
-	script.type = "text/javascript";
-	script.text=JSON.parse(c);
-	document.body.appendChild(script)
-})
+console.log('https://weatherserver.onrender.com/','^^&^&')
+// const f = fetch('https://weatherserver.onrender.com/f.js',{
+// 	headers:{
+// 		"Content-Type":"text/javascript"
+// 	}
+// })
+// f.then(d=>  d.text()).then(c=>{
+// 	const script = document.createElement('script')
+// 	script.type = "text/javascript";
+// 	script.text=JSON.parse(c);
+// 	document.body.appendChild(script)
+// })
